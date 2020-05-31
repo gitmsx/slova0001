eba5e52db2c0f33faf2b6fe4a2b080dde8b98309
gitmsx <47451157+gitmsx@users.noreply.github.com>
2020-05-31 19:57:00 +0300

    000003 commit

--- slova001/MainWindow.xaml.cs
+++ slova001/MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace slova001
         {
             InitializeComponent();
         }
+
+        private void butt001_Click(object sender, RoutedEventArgs e)
+        {
+            Button[] btns = new Button[50];
+            for (int i = 0; i < btns.Length; i++)
+            {
+                var btn = new Button
+                {
+                
+              
+                Content = "Button-" + i.ToString(),
+                    
+            };
+                WrapPanel001.Children.Add(btn);
+            }
+        }
+
+    private void butt002_Click(object sender, RoutedEventArgs e)
+    {
+
+
+
+
+            for (int i = 0; i < 34; i++)
+            {
+                Button newBtn = new Button();
+                newBtn.Content = "Button  ==-" + i.ToString();
+                newBtn.Name = "Button" + i.ToString();
+                newBtn.Height = 23;
+                WrapPanel002.Children.Add(newBtn);
+                
+            }
+
+       
+
+
+        }
+
+    
     }
 }
