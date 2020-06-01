34bfa2f66c54e0c4e8d769a714e439d07fdc9bfa
gitmsx <47451157+gitmsx@users.noreply.github.com>
2020-06-01 14:52:58 +0300

    0000066 commit

--- slova001/MainWindow.xaml.cs
+++ slova001/MainWindow.xaml.cs
@@ -13,25 +13,44 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Speech.Synthesis;
+using System.Drawing;
+
 
 
 namespace slova001
 {
-   
-    
-
 
     public partial class MainWindow : Window
     {
 
- private int kolichestvo_slov = 0;
-    private int vneseno_slov = 0;
+        private string but_ch = "but_cho";
+
+
+        private int kolichestvo_slov = -1;
+        private int vneseno_slov = 0;
 
 
         string[] massiv_input = new String[75];
+        int[] massiv_activ_buttons = new int[75];
+
+
+
+
         string[] massiv_etalon = new String[75];
         string[] massiv__ouput = new String[75];
         Label[] massiv__ouput_link = new Label[75];
+
+
+
+        Brush Brush_set_l101 = Brushes.Red;
+        Brush Brush_set_l102 = Brushes.SteelBlue;
+
+
+
+
+
+
+
 
 
 
@@ -55,11 +74,11 @@ namespace slova001
             {
                 var btn = new Button
                 {
-                
-              
-                Content = "Button-" + i.ToString(),
-                    
-            };
+
+
+                    Content = "Button-" + i.ToString(),
+
+                };
                 WrapPanel001.Children.Add(btn);
             }
         }
@@ -89,22 +108,42 @@ namespace slova001
         }
 
 
-        private void butt002_Click(object sender, RoutedEventArgs e)
-    {
+        private void new_message_in_words_in_panel_choice(object sender, RoutedEventArgs e)
+        {
+
+            double left = 1, top = 2, right = 3, bottom = 4;
+            Thickness myThickness = new Thickness();
+            myThickness.Bottom = 5;
+            myThickness.Left = 5;
+            myThickness.Right = 5;
+            myThickness.Top = 5;
 
 
+            WrapPanel002.Children.Clear();
             String inputfr = load_from_txt();
             zagruzka_frazy(" " + inputfr);
 
 
-
-
-            for (int i = 1; i <  massiv_input.Length; i++)
+            for (int i = 1; i < massiv_input.Length; i++)
             {
+
+                massiv_activ_buttons[i] = 1;
                 Button newBtn = new Button();
                 newBtn.Content = massiv_input[i];
-                newBtn.Name = "Button" + i.ToString();
-                newBtn.Height = 23;
+                newBtn.Name = but_ch + i.ToString();
+                newBtn.FontSize = slider_size.Value;
+
+                newBtn.Width = newBtn.Width + 7;
+                newBtn.Height = newBtn.Height + 3;
+
+
+                newBtn.BorderThickness = myThickness;
+                newBtn.BorderBrush = Brush_set_l101;
+
+
+                //MyControl.Margin = new Padding(0, 0, 0, 0);
+
+
                 newBtn.Click += new RoutedEventHandler(newBtn_Click);
                 WrapPanel002.Children.Add(newBtn);
 
@@ -116,12 +155,83 @@ namespace slova001
 
 
         }
+
+
+        private void add_word_in_answer(int position)
+        {
+
+
+            double left = 1, top = 2, right = 3, bottom = 4;
+
+
+            Thickness myThickness = new Thickness();
+            myThickness.Bottom = 5;
+            myThickness.Left = 5;
+            myThickness.Right = 5;
+            myThickness.Top = 5;
+
+
+
+
+
+            //        massiv_activ_buttons2 [position] = 1;
+
+            Button newBtn = new Button();
+            newBtn.Content = massiv_input[position];
+            newBtn.Name = "ButtonW" + position.ToString();
+            newBtn.FontSize = slider_size.Value;
+
+            newBtn.Width = newBtn.Width + 7;
+            newBtn.Height = newBtn.Height + 3;
+
+
+            newBtn.BorderThickness = myThickness;
+            newBtn.BorderBrush = Brush_set_l101;
+
+
+            //MyControl.Margin = new Padding(0, 0, 0, 0);
+
+
+            // newBtn.Click += new RoutedEventHandler(newBtn_Click2);
+            WrapPanel001.Children.Add(newBtn);
+
+
+        }
+
+
+
+
         private void newBtn_Click(object sender, RoutedEventArgs e)
         {
             var btn = sender as Button;
             if (btn != null)
             {
-                MessageBox.Show(btn.Name);
+                //MessageBox.Show(btn.Name);
+
+                string but_cho_str = btn.Name;
+                string but_cho_str2 = but_cho_str.Replace(but_ch, "");
+
+
+                int numb_but_pressed = Convert.ToInt32(but_cho_str2); 
+
+
+                if (massiv_activ_buttons[numb_but_pressed] == 1)
+                {
+                    massiv_activ_buttons[numb_but_pressed] = 0;
+                    btn.Background = Brush_set_l102;
+                    add_word_in_answer(numb_but_pressed);
+
+                }
+                else
+                {
+                    massiv_activ_buttons[numb_but_pressed] = 1;
+                    btn.Background = Brush_set_l101;
+
+                }
+
+
+
+
             }
         }
 
@@ -131,7 +241,7 @@ namespace slova001
             string[] lines = System.IO.File.ReadAllLines(@"T_eng.txt");
             string[] line_s_eng2 = System.IO.File.ReadAllLines(@"T_eng2.txt", Encoding.GetEncoding(1251));
 
-            string[] line_s_rus  = System.IO.File.ReadAllLines(@"T_rus.txt", Encoding.GetEncoding(1251));
+            string[] line_s_rus = System.IO.File.ReadAllLines(@"T_rus.txt", Encoding.GetEncoding(1251));
             string[] line_s_rus2 = System.IO.File.ReadAllLines(@"T_rus2.txt", Encoding.GetEncoding(1251));
 
             var rand001 = new Random();
@@ -156,7 +266,7 @@ namespace slova001
             SpeechSynthesizer synth = new SpeechSynthesizer();
 
 
-            
+
 
             synth.Rate = Convert.ToInt32(sliderspeed.Value);
             synth.Volume = Convert.ToInt32(slidervol.Value);
